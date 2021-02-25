3997ac1d7af8ea4ffb9f14357563eec6589762c3
wojo321 <wojtek.f.99@gmail.com>
2021-02-25 14:58:46 +0100

    init

---
+++ data.js
@@ -0,0 +1,27 @@
+module.exports = function () {
+    return {
+    products: [
+    { id: 1, name: "Kajak", category: "Sporty wodne",
+    description: "Łódka przeznaczona dla jednej osoby.", price: 275 },
+    { id: 2, name: "Kamizelka ratunkowa", category: "Sporty wodne",
+    description: "Chroni i dodaje uroku.", price: 48.95 },
+    { id: 3, name: "Piłka", category: "Piłka nożna",
+    description: "Zatwierdzone przez FIFA rozmiar i waga.", price: 19.50 },
+    { id: 4, name: "Flagi narożne", category: "Piłka nożna",
+    description: "Nadadzą twojemu boisku profesjonalny wygląd.",
+    price: 34.95 },
+    { id: 5, name: "Stadion", category: "Piłka nożna",
+    description: "Składany stadion na 35 000 osób.", price: 79500 },
+    { id: 6, name: "Czapka", category: "Szachy",
+    description: "Zwiększa efektywność mózgu o 75%.", price: 16 },
+    { id: 7, name: "Niestabilne krzesło", category: "Szachy",
+    description: "Zmniejsza szanse przeciwnika.",
+    price: 29.95 },
+    { id: 8, name: "Ludzka szachownica", category: "Szachy",
+    description: "Przyjemna gra dla całej rodziny.", price: 75 },
+    { id: 9, name: "Błyszczący król", category: "Szachy",
+    description: "Pokryty złotem i wysadzany diamentami król.", price: 1200 }
+    ],
+    orders: []
+    }
+   }
